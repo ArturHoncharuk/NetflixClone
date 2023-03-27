31eb4af25fef244ab2e376e98d5e45600196ce54
ArthurHoncharuk <Dotwork.Play@gmail.com>
2023-03-27 20:49:48 +0300

    updated-tab-navigation-bar

--- netflix/Controllers/HomeViewController.swift
+++ netflix/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  netflix
 //
 //  Created by Arthur Honcharuk on 27/03/2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
     }
 
-
 }
-
---
+++ netflix/Controllers/UpcomingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpcomingViewController.swift
+//  netflix
+//
+//  Created by Arthur Honcharuk on 27/03/2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
